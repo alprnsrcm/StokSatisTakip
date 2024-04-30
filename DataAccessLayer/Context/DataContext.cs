3f6b29ef9b8147cf8df83c93281f734fa11f3204
alprnsrcm <107485609+alprnsrcm@users.noreply.github.com>
2024-04-30 13:49:03 +0300

    Add project files.

---
+++ DataAccessLayer/Context/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Context
+{
+	public class DataContext : DbContext
+	{
+		public DataContext()
+		{
+		}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-G1CLG7U\\SQLEXPRESS;database=StokSatisTakipDb; integrated security=true;TrustServerCertificate=true;");
+        }
+
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}  
---
+++ StokSatisTakip/Controllers/AdminCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StokSatisTakip.Controllers
+{
+    public class AdminCategory : Controller
+    {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        public IActionResult Index()
+        {
+            return View(categoryRepository.List());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Create(Category p)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepository.Insert(p);
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("","Bir Hata Oluşstu");
+            return View();
+        }
+    }
+}
